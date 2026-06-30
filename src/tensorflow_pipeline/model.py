1ee321737d0a3a0b921c8def90aaeec0a51e828e
Roberto San Miguel <roberto.san1332@myunfc.ca>
2026-06-30 19:06:19 -0400

    Add TensorFlow EfficientNetB0 model

---
+++ src/tensorflow_pipeline/model.py
@@ -0,0 +1,43 @@
+"""
+TensorFlow EfficientNetB0 model definition.
+"""
+
+import tensorflow as tf
+
+from .config import IMAGE_SIZE, LEARNING_RATE
+
+
+def build_efficientnet_model(num_classes: int):
+    """
+    Build an EfficientNetB0 transfer learning model.
+    """
+
+    base_model = tf.keras.applications.EfficientNetB0(
+        include_top=False,
+        weights="imagenet",
+        input_shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3)
+    )
+
+    base_model.trainable = False
+
+    inputs = tf.keras.Input(shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3))
+
+    x = tf.keras.applications.efficientnet.preprocess_input(inputs)
+    x = base_model(x, training=False)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+
+    outputs = tf.keras.layers.Dense(
+        num_classes,
+        activation="softmax"
+    )(x)
+
+    model = tf.keras.Model(inputs, outputs)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
