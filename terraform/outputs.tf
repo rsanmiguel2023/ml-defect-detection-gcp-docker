6b6c86abbc914d5b8107ac038287e00f2799d822
Roberto San Miguel <roberto.san1332@myunfc.ca>
2026-07-03 23:45:02 -0400

    Provision Artifact Registry with Terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,13 @@
 #
 # Outputs
 #
+
+output "artifact_registry_repository" {
+  description = "Artifact Registry repository name"
+  value       = google_artifact_registry_repository.docker_repository.name
+}
+
+output "artifact_registry_location" {
+  description = "Artifact Registry repository location"
+  value       = google_artifact_registry_repository.docker_repository.location
+}
